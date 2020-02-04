c62e6c159bad08c9a62bdf4e6e5cc9b7c37f41c7
Julia Damerow <julia.damerow@gmail.com>
2020-02-04 13:49:33 -0500

    Adjustments for release in Pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='example-lfuncs',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='example-lfuncs',
       version='0.1',
       description='A few functions to teach Python and PIP',
       url='https://github.com/diging-training/example-package-lfuncs',
       author='jdamerow',
       author_email='jdamerow@asu.edu',
-      license='MPL-2.0',
-      packages=['lfuncs'],
-      zip_safe=False)
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6')
