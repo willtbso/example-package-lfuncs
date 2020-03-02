b0d5a5b466134ce803d298852f8931390c465623
Julia Damerow <julia.damerow@gmail.com>
2020-03-02 13:39:56 -0500

    added documentation

--- lfuncs/__init__.py
+++ lfuncs/__init__.py
@@ -1,10 +1,25 @@
 def find_startswith_in_list(prefix, list1):
+    """Filters the given list so that only elements that
+    start with the provided prefix will be returned.
+    """
     return list(filter(lambda e: e.startswith(prefix), list1))
 
 def find_endswith_in_list(suffix, list1):
+    """Filters the given list so that only elements that
+    end with the provided suffix will be returned.
+    """
     return list(filter(lambda e: e.endswith(suffix), list1))
 
 def split_lists(olist, prefix):
+    """Splits a list into two lists and returns them as a tuple.
+    The first list will contain all elements in the original list
+    that start with the provided prefix. The second list will
+    contain all other elements.
+
+    Parameters:
+    - olist: the list that should be separated into two lists
+    - prefix: the prefix that should be used to separate the list
+    """
     list1 = []
     list2 = []
     for e in olist:
@@ -16,4 +31,5 @@ def split_lists(olist, prefix):
     return list1, list2
 
 def joke():
+    """Prints a funny joke."""
     print('A foo walks into a bar, takes a look around and says "Hello World!"')
